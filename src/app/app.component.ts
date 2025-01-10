3dd3e3bc8a6bdec739bc95cfe5ed8e3658df1f01
AxelLeo129 <axelleo129dev@gmail.com>
2025-01-10 11:47:58 -0600

    Project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  active = 1;
   title = 'angular-17-example';
 }
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  showNavigationArrows = false;
+	showNavigationIndicators = false;
+	images = [1055, 194, 368].map((n) => `https://picsum.photos/id/${n}/1500/500`);
+
+	constructor(config: NgbCarouselConfig) {
+		// customize default values of carousels used by this component tree
+		config.showNavigationArrows = true;
+		config.showNavigationIndicators = true;
+	}
+
+}
